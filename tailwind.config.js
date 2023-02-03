64bc5a8b7e574a28aa8ea69c5c2418d68491903d
Lucas Bandeira <lucas4workl8tr@gmail.com>
2023-02-03 20:06:46 -0300

    projeto em next criado

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#FFFFFF',
+      black: '#000000',
+      lightBlack: '#585858',
+      red: '#ff0000',
+      green: {
+        background: '#ABCC99',
+        button: '#39C516',
+        buttonLight: '#75d55d',
+        active: '#43992E',
+        header: '#43592D',
+        menu: '#29332D',
+        background2: '#313d37'
+      },
+      gray: {
+        background: '#DADADA',
+        button: '#41564B',
+        active: '#161A17',
+        inactive: '#ADACAB'
+      },
+      orange: '#e58007'
+    }
+  },
+  plugins: [],
+}
